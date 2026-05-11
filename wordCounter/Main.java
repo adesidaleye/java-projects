7d1258de100ce75113a17f9090eed6db96904e61
Adesida Leye <adeleyeadesida@gmail.com>
2026-05-11 04:05:28 +0100

    simple word counter

--- wordCounter/Main.java
+++ wordCounter/Main.java
@@ -1,5 +1,21 @@
 package wordCounter;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter sentence(s) or paragraph(s): ");
+
+        int counter = 0;
+        String word = sc.nextLine();
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == ' ') continue;
+            counter++;
+        }
+
+        System.out.printf("There are %d words in the text.\n", counter);
+        sc.close();
+    }
 }
